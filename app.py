588f45c6fc3e064ed88188b25a6217cfa726263a
Sten <102556636+zacharst@users.noreply.github.com>
2025-09-25 22:59:26 +0200

    Removed "SQL-Filter" Tab. The functionality is now used by "Tabelle Anzeigen"

--- app.py
+++ app.py
@@ -78,32 +78,23 @@ def main():
 
     ## Nur Verwaltung und Kursleiter kriegen SQL-Abfrage und SQL-Filter angezeigt
     if st.session_state["logged_in"]:
-        tabs = ["Tabelle anzeigen", "SQL-Abfrage", "SQL-Filter","Tabelle bearbeiten"]
+        tabs = ["Tabelle anzeigen", "SQL-Abfrage","Tabelle bearbeiten"]
         active_tab = st.radio("Wähle einen Tab", tabs, index=0)
     else:
-        tabs = ["Tabelle anzeigen", "SQL-Filter"]
+        tabs = ["Tabelle anzeigen"]
         active_tab = st.radio("Wähle einen Tab", tabs, index=0)
         
 
     with st.sidebar:
         selected_table, filters, limit_active, default_limit, df_for_filters = show_sidebar(conn, active_tab)
 
-    if active_tab == "Tabelle anzeigen":
-        st.title("Tabelle anzeigen")
-        if df_for_filters is None:
-            st.info("Bitte wähle eine Tabelle in der Sidebar.")
-        else:
-            limit_to_use = default_limit if limit_active else None
-            filtered_df = apply_filters(
-                df_for_filters, filters, limit=limit_to_use if limit_to_use else len(df_for_filters)
-            )
-            display_dataframe(filtered_df)
+   
 
-    elif active_tab == "SQL-Abfrage":
+    if active_tab == "SQL-Abfrage":
         st.title("Freie SQL-Abfrage")
         run_custom_query()
 
-    elif active_tab == "SQL-Filter":
+    elif active_tab == "Tabelle anzeigen":
         limit_to_use = default_limit if limit_active else None
         st.title("SQL-Filter")
         if df_for_filters is None or selected_table is None:
